b9ee01b35d6fbd655702100c5037576c475d79c8
JUNHUA93 <tomdong93@gmail.com>
2017-12-30 11:35:49 -0600

    Update js.js

--- js/js.js
+++ js/js.js
@@ -3,6 +3,24 @@ function info()
     document.getElementById('myimage').src = "project-2-images/iitcamp.jpg";
 }
 
+function validation()
+{
+var a = document.form.name.value;
+var b = document.form.password.value;
+if(a=="")
+{
+alert("Please Enter Your Name");
+document.form.name.focus();
+return false;
+}
+
+if(b=="")
+{alert("Please Enter the password");
+document.form.password.focus();
+return false;
+}
+}
+
 function stoplogo() {
     document.getElementById('myimage').src = "project-2-images/hawklogo.jpg";
 }
